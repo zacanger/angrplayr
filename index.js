434eab73fdeb2da054c9ca719a6df7111aaf8eda
zacanger <zac@zacanger.com>
2016-12-02 15:47:35 -0700

    Here is a file.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 
-console.log('THERE IS NOTHING HERE YET')
-console.log('I AM SORRY')
+const
+  fs = require('fs')
+, Lame = require('lame')
+, path = require('path')
+, Speaker = require('speaker')
+, args = process.argv.slice(2)
+, getFiles = (list) => list.map(item => path.resolve(item))
+, theFiles = getFiles(args)
+
+theFiles.forEach(file => {
+  fs.createReadStream(file)
+  .pipe(new Lame.Decoder())
+  .pipe(new Speaker())
+})
