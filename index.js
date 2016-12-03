ab569d39729af411611b8b2af621794d90841d2e
zacanger <zac@zacanger.com>
2016-12-03 14:21:15 -0700

    Start getting stuff in place to play multiple songs.

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+const { createReadStream } = require('fs')
+const { resolve } = require('path')
 const Lame = require('lame')
-const path = require('path')
 const Speaker = require('speaker')
-// const args = process.argv[2]
 const args = process.argv.slice(2)
-if (!args) process.exit(0)
-
-const getFiles = (list) => list.map(item => path.resolve(item))
+if (!args || (args && !args.length)) process.exit(0)
+const getFiles = (list) => list.map((el) => resolve(el))
 const theFiles = getFiles(args)
 
-theFiles.forEach(file => {
-  process.stdout.write(`>> ${file}\n`)
-  fs.createReadStream(file)
-  .pipe(new Lame.Decoder())
-  .pipe(new Speaker())
-})
+const theStuff = (file) => (
+  new Promise((resolve, reject) => {
+    process.stdout.write(`>> ${file}\n`)
+    createReadStream(file)
+    .pipe(new Lame.Decoder())
+    .pipe(new Speaker())
+    .on('end', resolve)
+    .on('close', () => {})
+  })
+)
+
+function * doTheThings (songs) {
+  while (songs.length) {
+    try {
+      yield theStuff(songs[0])
+      if (songs.length) songs.shift()
+      else return
+    } catch (e) {
+      return console.error('EVERYTHING IS BROKEN OH GOD', e)
+    }
+  }
+}
+
+const makeTheThing = (gen) => (arg) => {
+  const a = gen(arg)
+  while (!a.next().done) a.next()
+}
+
+const main = makeTheThing(doTheThings)
+
+main(theFiles)
