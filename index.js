808751af905d23c9e3634a11e5e159e9cd758f00
zacanger <zac@zacanger.com>
2017-04-30 12:22:27 -0600

    Use zeelib for small node utils.

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 
 const StreamPlayer = require('stream-player')
 const player = new StreamPlayer()
-const { parse, resolve } = require('path')
-const args = process.argv.slice(2)
-if (!args || (args && !args.length)) process.exit(0)
-const getFiles = (list) => list.map((el) => resolve(el))
-const theFiles = getFiles(args)
-
-const getBasename = (a) => parse(a).base
+const { exit, getArgs, getBasename, resolveFiles } = require('zeelib')
+const args = getArgs()
+if (!args || (args && !args.length)) exit(0)
+const theFiles = resolveFiles(args)
 
 theFiles.forEach((file) => {
   player.add(file, getBasename(file))
