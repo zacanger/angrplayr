58a07bef8616f7fae29abdd81af38de57c61a420
zacanger <zac@zacanger.com>
2017-08-24 14:05:02 -0600

    This can be a thing again, I guess

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 
 const StreamPlayer = require('stream-player')
 const player = new StreamPlayer()
-const { exit, getArgs, getBasename, resolveFiles } = require('zeelib')
+const exit = require('zeelib/lib/exit').default
+const getArgs = require('zeelib/lib/get-args').default
+const getBasename = require('zeelib/lib/get-basename').default
+const resolveFiles = require('zeelib/lib/resolve-files').default
 const args = getArgs()
-if (!args || (args && !args.length)) exit(0)
+
+if (module.parent) {
+  console.log('please use the `ap` command!')
+  exit(1)
+}
+
+if (!args || (args && !args.length)) {
+  console.log('usage: ap somefile.mp3')
+  exit(0)
+}
+
 const theFiles = resolveFiles(args)
 
 theFiles.forEach((file) => {
